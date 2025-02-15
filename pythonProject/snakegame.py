b3bbecd11a89b6ec754ab9a59980756ddd5e4a17
batontapok <jorikbacokpatik@gmail.com>
2025-02-15 14:17:28 +0900

    3
    Добавлена система рекордов и текущего счета

--- pythonProject/snakegame.py
+++ pythonProject/snakegame.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+import os
 
 # Инициализация Pygame
 pygame.init()
@@ -31,16 +32,35 @@ snake_speed = 15
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
+# Файл для хранения рекорда
+record_file = 'snake_record.txt'
+
+def get_high_score():
+    if os.path.exists(record_file):
+        with open(record_file, 'r') as file:
+            try:
+                return int(file.read().strip())
+            except ValueError:
+                return 0
+    return 0
+
+def save_high_score(score):
+    with open(record_file, 'w') as file:
+        file.write(str(score))
 
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
-
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
+def your_score(score, high_score):
+    value = score_font.render(f"Ваш счёт: {score}", True, yellow)
+    high_value = score_font.render(f"Рекорд: {high_score}", True, yellow)
+    dis.blit(value, [0, 0])
+    dis.blit(high_value, [dis_width - high_value.get_width(), 0])
 
 def gameLoop():
     game_over = False
@@ -57,6 +77,8 @@ def gameLoop():
 
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+    high_score = get_high_score()
 
     while not game_over:
 
@@ -92,6 +114,10 @@ def gameLoop():
 
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
+            if Length_of_snake - 1 > high_score:
+                save_high_score(Length_of_snake - 1)
+                high_score = Length_of_snake - 1
+
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
@@ -106,8 +132,12 @@ def gameLoop():
         for x in snake_List[:-1]:
             if x == snake_Head:
                 game_close = True
+                if Length_of_snake - 1 > high_score:
+                    save_high_score(Length_of_snake - 1)
+                    high_score = Length_of_snake - 1
 
         our_snake(snake_block, snake_List)
+        your_score(Length_of_snake - 1, high_score)
 
         pygame.display.update()
 
@@ -120,6 +150,5 @@ def gameLoop():
 
     pygame.quit()
     quit()
-
 
 gameLoop()
